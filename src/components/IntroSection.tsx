16ba484449b095936850b372dd740fdfd257cb71
sahelo7 <it@sahel.ch>
2026-08-01 06:16:42 +0000

    Test

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -52,6 +52,15 @@ const IntroSection: React.FC = () => {
                                 >
                                     Join Our Hub
                                 </Link>
+                                                                <Link
+                                    to="PDF"
+                                    smooth={true}
+                                    duration={scrollDuration}
+                                    offset={scrollOffset}
+                                    className="px-6 py-3 bg-neutral-white/20 backdrop-blur-sm text-neutral-white border border-neutral-white/40 font-medium rounded-lg hover:bg-neutral-white/30 transition-all duration-200 cursor-pointer w-full md:w-auto"
+                                >
+                                    PDF
+                                </Link>
                             </div>
                         </div>
                         
