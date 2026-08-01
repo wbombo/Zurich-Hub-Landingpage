61478ee1c563747ed8522958b86f5f0d945eee2d
sahelo7 <it@sahel.ch>
2026-08-01 05:44:36 +0000

    Dates

--- src/components/RecruitmentSection.tsx
+++ src/components/RecruitmentSection.tsx
@@ -52,8 +52,7 @@ const RecruitmentSection: React.FC = () => {
         hubConfig?.recruitmentText.additionalInfo || "Supporting one of our projects as a volunteer or participating in one of our events before your application is considered a huge plus during the recruitment process. Please check out our social media channels or reach out to our Members Officer to learn more about our current projects and events.",
         "We recruit in two annual cycles. Applications submitted by 31 January are considered in the first cycle, and those submitted by 31 August are considered in the second cycle. After an applicant made it through our first screening round, an in-person recruiting event will follow after 2-4 weeks of the application cut-off date. Respective candidate will be informed via Email.",
         <div className="flex flex-col gap-1 mt-1">
-            Recruitment Interviews dates:
-            <span>
+            <strong className="font-bold">Recruitment Interviews dates:</strong>            <span>
                 • 22 September Tuesday 7–9pm:{" "}
                 <a
                 href="https://luma.com/ta0y9qf6"
